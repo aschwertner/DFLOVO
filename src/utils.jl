6e56b81b8419eb3d2c56184b1fb923ad3323e751
Anderson Schwertner <andersonschwertner@hotmail.com>
2021-07-25 19:27:49 -0300

    Adds 'verify_initial_room' function

--- src/utils.jl
+++ src/utils.jl
@@ -2,3 +2,37 @@
 # Set of functions useful for the execution of the main algorithm. 
 #-------------------------------------------------------------------------------
 
+"""
+
+    verify_initial_room(n::Int64, Δ::Float64, a::Vector{Float64},
+                        b::Vector{Float64})
+
+Checks whether the limits satisfy the conditions 'b' >= 'a' + 2 * 'Δ'.
+
+    - 'n': dimension of the search space.
+
+    - 'Δ': radius of the trust-region.
+
+    - 'a': n-dimensional vector with the lower bounds.
+
+    - 'b': n-dimensional vector with the upper bounds.
+
+Returns a Boolean value.
+
+"""
+function verify_initial_room(
+                                n::Int64, 
+                                Δ::Float64, 
+                                a::Vector{Float64}, 
+                                b::Vector{Float64}
+                                )
+
+    for i = 1:n
+        if ( b[i] - a[i] ) < ( 2.0 * Δ )
+            return false 
+        end
+    end
+
+    return true
+
+end
