3d4513bbe6adb476c5e6d010f41ba50f042b58ad
Anderson Schwertner <andersonschwertner@hotmail.com>
2021-07-25 20:56:59 -0300

    Adds 'correct_guess_bounds' function

--- src/utils.jl
+++ src/utils.jl
@@ -36,3 +36,67 @@ function verify_initial_room(
     return true
 
 end
+
+"""
+
+    correct_guess_bounds!(n::Int64, δ::Float64, a::Vector{Float64},
+                            b::Vector{Float64}, x::Vector{Float64},
+                            ao::Vector{Float64}, bo::Vector{Float64})
+
+Fixes initial guess 'x' and sets the lower and upper bound vectors 'ao' and 'bo'
+shifted to the origin 'x'.
+
+    - 'n': dimension of the search space.
+
+    - 'δ': radius of the sample set.
+
+    - 'a': n-dimensional vector with the lower bounds.
+
+    - 'b': n-dimensional vector with the upper bounds.
+
+    - 'x': n-dimensional vector (initial guess).
+
+    - 'ao': n-dimensional vector with the shifted lower bounds.
+
+    - 'bo': n-dimensional vector with the shifted upper bounds.
+
+Modifies the vectors 'x', 'ao', 'bo'.
+
+"""
+function correct_guess_bounds!( 
+                                n::Int64, 
+                                δ::Float64, 
+                                a::Vector{Float64}, 
+                                b::Vector{Float64},
+                                x::Vector{Float64},
+                                ao::Vector{Float64}, 
+                                bo::Vector{Float64}
+                                )
+
+    for i=1:n
+        ao[i] = a[i] - x[i]
+        bo[i] = b[i] - x[i]
+        if ao[i] >= - δ
+            if ao[i] >= 0.0
+                x[i] = a[i]
+                ao[i] = 0.0
+                bo[i] = b[i] - a[i]
+            else
+                x[i] = a[i] + δ
+                ao[i] = - δ
+                bo[i] = max( b[i] - x[i], δ )
+            end
+        elseif bo[i] <= δ
+            if bo[i] <= 0.0
+                x[i] = b[i]
+                ao[i] = a[i] - b[i]
+                bo[i] = 0.0
+            else
+                x[i] = b[i] - δ
+                ao[i] = min( a[i] - x[i], - δ )
+                bo[i] = δ
+            end
+        end
+    end
+
+end
