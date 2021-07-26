4fd579b91ac31a01530671b0be09dc6d15a38a37
Anderson Schwertner <andersonschwertner@hotmail.com>
2021-07-26 10:10:51 -0300

    Adds 'runtests.jl' file

---
+++ test/runtests.jl
@@ -0,0 +1,12 @@
+#-------------------------------------------------------------------------------
+# Run test files
+#-------------------------------------------------------------------------------
+
+# Adds modules
+using DFLOVO
+using Test
+using LinearAlgebra
+
+# Adds files
+include("test_lovo_utils.jl")
+include("test_utils.jl")
