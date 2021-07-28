e3585560ddb88ded840c03330c7ccfd2f3b0a356
Anderson Schwertner <andersonschwertner@hotmail.com>
2021-07-28 13:01:57 -0300

    Verify algorithm initialization conditions

--- src/DFLOVO.jl
+++ src/DFLOVO.jl
@@ -1,15 +1,66 @@
 # Turn off precompilation during development
 __precompile__(false)
 
+#-------------------------------------------------------------------------------
 # Main file implementing DFLOVO in Julia
+#-------------------------------------------------------------------------------
 
 module DFLOVO
 
-    import Base: (*)
+    # Load dependencies
+    using Base: Float64
+import Base: (*)
     using LinearAlgebra
 
-    #Adds files
+    # Load code
     include("lovo_utils.jl")
     include("utils.jl")
+
+    function dflovo(
+                    func_list::Array{Function,1},
+                    x::Vector{Float64},
+                    a::Vector{Float64}, 
+                    b::Vector{Float64},
+                    δ::Float64,
+                    Δ::Float64;
+                    m::Int64=(2 * length(x) + 1),
+                    itmax::Int64=5000,
+                    maxfun::Int64=(1000 * (length(func_list) + m)),
+                    Γmax::Int64=1,
+                    β::Float64=1.0,
+                    τ1::Float64=0.6,
+                    τ2::Float64=1.5,
+                    τ3::Float64=2.0,
+                    η::Float64=0.1,
+                    η1::Float64=0.3,
+                    η2::Float64=0.6
+                    )
+
+        # Calculates the search space dimension.
+        n = length(x)
+
+        # Calculates the number of functions that make up the objective function fmin
+        r = length(func_list)
+
+        # Checks initial algorithm initialization conditions.
+        m_min = n + 2
+        m_max = convert(Int64, ( n + 1 ) * ( n + 2 ) / 2)
+        @assert m_min ≤ m ≤ m_max "The number of interpolation points 'm' must satisfy m ∈ [$(m_min), $(m_max)]."
+        @assert length(a) == n "The vector 'a' must have dimension $(n)."
+        @assert length(b) == n "The vector 'b' must have dimension $(n)."
+        @assert 0 < δ ≤ Δ "The radius of the sample set 'δ' must be positive and less or equal to the trust-region radius 'Δ'."
+        @assert verify_initial_room(n, δ, a, b) "The radius of the initial sample set 'δ' is not suitable, it must satisfy a[i]- b[i] >= 2δ, for i = 1, ..., $(n)."
+        @assert Γmax > 0 "The parameter 'Γmax' must be positive."
+        @assert β > 0 "The parameter 'β' must be positive."
+        @assert 0 < τ1 < 1 "The parameter 'τ1' must be positive and less than one."
+        @assert 1 ≤ τ2 "The parameter 'τ2' must be greater than or equal to one."
+        @assert τ2 ≤ τ3 "The parameter 'τ3' must be greater than or equal to 'τ2'."
+        @assert 0 < η1 < 1 "The parameter 'η1' must be positive and less than one."
+        @assert 0 ≤ η < η1 "The parameter 'η' must be nonnegative and less than 'η1'."
+        @assert η1 ≤ η2 "The parameter 'η2' must be greater than or equal to 'η1'."
+        
+
+ 
+    end
 
 end
