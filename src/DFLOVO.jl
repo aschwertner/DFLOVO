4d9a684ab99cb2cd3f0e60edfb5aec554840561a
Anderson Schwertner <andersonschwertner@hotmail.com>
2021-07-28 13:11:27 -0300

    Adds a parameter for printing information

--- src/DFLOVO.jl
+++ src/DFLOVO.jl
@@ -33,7 +33,8 @@ import Base: (*)
                     τ3::Float64=2.0,
                     η::Float64=0.1,
                     η1::Float64=0.3,
-                    η2::Float64=0.6
+                    η2::Float64=0.6,
+                    verbose::Bool=true
                     )
 
         # Calculates the search space dimension.
@@ -42,20 +43,20 @@ import Base: (*)
         # Calculates the number of functions that make up the objective function fmin
         r = length(func_list)
 
-        # Checks initial algorithm initialization conditions.
+        # Verify algorithm initialization conditions.
         m_min = n + 2
         m_max = convert(Int64, ( n + 1 ) * ( n + 2 ) / 2)
         @assert m_min ≤ m ≤ m_max "The number of interpolation points 'm' must satisfy m ∈ [$(m_min), $(m_max)]."
         @assert length(a) == n "The vector 'a' must have dimension $(n)."
         @assert length(b) == n "The vector 'b' must have dimension $(n)."
-        @assert 0 < δ ≤ Δ "The radius of the sample set 'δ' must be positive and less or equal to the trust-region radius 'Δ'."
+        @assert 0.0 < δ ≤ Δ "The radius of the sample set 'δ' must be positive and less or equal to the trust-region radius 'Δ'."
         @assert verify_initial_room(n, δ, a, b) "The radius of the initial sample set 'δ' is not suitable, it must satisfy a[i]- b[i] >= 2δ, for i = 1, ..., $(n)."
         @assert Γmax > 0 "The parameter 'Γmax' must be positive."
-        @assert β > 0 "The parameter 'β' must be positive."
-        @assert 0 < τ1 < 1 "The parameter 'τ1' must be positive and less than one."
-        @assert 1 ≤ τ2 "The parameter 'τ2' must be greater than or equal to one."
+        @assert β > 0.0 "The parameter 'β' must be positive."
+        @assert 0.0 < τ1 < 1.0 "The parameter 'τ1' must be positive and less than one."
+        @assert 1.0 ≤ τ2 "The parameter 'τ2' must be greater than or equal to one."
         @assert τ2 ≤ τ3 "The parameter 'τ3' must be greater than or equal to 'τ2'."
-        @assert 0 < η1 < 1 "The parameter 'η1' must be positive and less than one."
+        @assert 0.0 < η1 < 1.0 "The parameter 'η1' must be positive and less than one."
         @assert 0 ≤ η < η1 "The parameter 'η' must be nonnegative and less than 'η1'."
         @assert η1 ≤ η2 "The parameter 'η2' must be greater than or equal to 'η1'."
         
