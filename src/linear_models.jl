c43e00ed95caeefbc8d6e6c34705858e2562708d
Anderson Schwertner <andersonschwertner@hotmail.com>
2021-08-12 14:20:18 -0300

    Adds 'LinearModel' function

--- src/linear_models.jl
+++ src/linear_models.jl
@@ -3,16 +3,34 @@
 #-------------------------------------------------------------------------------
 
 struct LinearModel
-   
+
     imin  :: Int64
-    kopt  :: Int64
-    fbase :: Float64
     c     :: Float64
     g     :: AbstractVector
     xbase :: AbstractVector
-    xopt  :: AbstractVector
-    fval  :: AbstractVector    
+    fval  :: AbstractVector
     dst   :: AbstractVector
+    Y     :: AbstractMatrix
+
+end
+
+LinearModel(imin, xbase, fval, Y) = begin
+
+    m, n = size(Y)
+    dst = zeros(Float64, m)
+
+    for j =1:n
+        for i = 2:m
+            Y[i, j] -= xbase[j]
+            dst[i - 1] += Y[i, j] ^ 2.0
+        end
+    end
+    @. dst = sqrt(dst)
+    
+    g = A \ (fval[2:end] .- fval[1])
+    c = fval[1] - dot(g, xbase)
+
+    LinearModel(imin, c, g, xbase, fval, dst, Y)
 
 end
 
