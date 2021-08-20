3d976bc3ecaa4372bb2985797dccecb8b70d4f56
Anderson Schwertner <andersonschwertner@hotmail.com>
2021-08-20 17:38:02 -0300

    Adds 'create_output' function

--- src/lowder_output.jl
+++ src/lowder_output.jl
@@ -10,7 +10,20 @@ struct LOWDEROutput
 
 end
 
-###!!! Escrever uma função para criar a estrutura do LOWDEROutput
+function create_output(
+                        model::AbstractModel,
+                        nit::Int64,
+                        nf::Int64,
+                        exit_flag::Int64
+                        )
+
+    if model.kopt[] == 1
+        return LOWDEROutput(nit, model.imin[], nf, exit_flag, true, model.fval[1], model.xbase)
+    else
+        return LOWDEROutput(nit, model.imin[], nf, exit_flag, false, model.fval[model.kopt[]], model.xopt)
+    end
+
+end
 
 function show_output(
                         output::LOWDEROutput
