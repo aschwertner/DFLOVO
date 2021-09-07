3d308a105545851ffba5082a2dd4846700f0908c
Anderson Schwertner <andersonschwertner@hotmail.com>
2021-09-07 16:07:45 -0300

    Corrects the call of 'println' inside 'show_output' function

--- src/lowder_output.jl
+++ src/lowder_output.jl
@@ -23,12 +23,12 @@ function show_output(
                         )
     
     println("** LOWDER Output **")
-    println("Status (.status): ", $(output.status))
-    println("Solution (.solution): ", $(output.solution))
-    println("Number of iterations (.iter): ", $(output.iter))
-    println("Number of function evaluations (.nf): ", $(output.nf))
-    println("Objective function value (.f): ", $(output.f))
-    println("Trusted objective funtion value (.true_val): ", $(output.true_val))
-    println("Index of the function (.index): ", $(output.index))
+    println("Status (.status): $( output.status )" )
+    println("Solution (.solution): $( output.solution )" )
+    println("Number of iterations (.iter): $( output.iter )" )
+    println("Number of function evaluations (.nf): $( output.nf )" )
+    println("Objective function value (.f): $( output.f )" )
+    println("Trusted objective funtion value (.true_val): $( output.true_val )" )
+    println("Index of the function (.index): $( output.index )" )
 
 end
