42d7849cb5ab71bf0ccee29e4473e3b76e552e09
Anderson Schwertner <andersonschwertner@hotmail.com>
2021-09-15 16:30:50 -0300

    Corrects the call of the tests

--- test/test_lovo_utils.jl
+++ test/test_lovo_utils.jl
@@ -15,43 +15,51 @@
         
         # Test parameters
         y = [0.0, 0.0]
+        set = zeros(Bool, r)
         # Expected return
         fmin_return = 0.0
         imin_return = 1
         # Test
-        fmin, imin = LOWDER.fmin_eval(func_list, r, y)
+        fmin, imin = LOWDER.fmin_eval!(func_list, r, y, set)
         @test( fmin == fmin_return )
         @test( imin == imin_return )
+        @test( set == [true, true, false, true] )
 
         # Test parameters
         y = [0.0, 1.0]
+        set = zeros(Bool, r)
         # Expected return
         fmin_return = -1.0
         imin_return = 2
         # Test
-        fmin, imin = LOWDER.fmin_eval(func_list, r, y)
+        fmin, imin = LOWDER.fmin_eval!(func_list, r, y, set)
         @test( fmin == fmin_return )
         @test( imin == imin_return )
+        @test( set == [false, true, false, false] )
 
         # Test parameters
         y = [1.0, 1.0]
+        set = zeros(Bool, r)
         # Expected return
         fmin_return = -1.0
         imin_return = 3
         # Test
-        fmin, imin = LOWDER.fmin_eval(func_list, r, y)
+        fmin, imin = LOWDER.fmin_eval!(func_list, r, y, set)
         @test( fmin == fmin_return )
         @test( imin == imin_return )
+        @test( set == [false, false, true, false] )
 
         # Test parameters
         y = [0.0, - 1.0]
+        set = zeros(Bool, r)
         # Expected return
         fmin_return = sin(- 1.0)
         imin_return = 4
         # Test
-        fmin, imin = LOWDER.fmin_eval(func_list, r, y)
+        fmin, imin = LOWDER.fmin_eval!(func_list, r, y, set)
         @test( fmin == fmin_return )
         @test( imin == imin_return )
+        @test( set == [false, false, false, true] )
 
     end
 
@@ -101,69 +109,96 @@
 
     end 
 
-    @testset "verify_index_imin" begin
+    @testset "fmin_partial_eval" begin
 
         f(x) = ( x[1] + x[2] ) ^ 2.0
         g(x) = x[1] - x[2]
         h(x) = 1.0 - x[1] - x[2]
         l(x) = sin( x[1] + x[2] )
         func_list = [f, g, h, l]
+        r = 4
+        set = zeros(Bool, r)
 
         # Test parameters
         y = [0.0, 0.0]
-        fmin_y = 0.0
-        # Expected return
-        test_1_return = true
-        test_2_return = true
-        test_3_return = false
-        test_4_return = true
-        # Test
-        @test( LOWDER.verify_index_imin(func_list, 1, fmin_y, y) == test_1_return )
-        @test( LOWDER.verify_index_imin(func_list, 2, fmin_y, y) == test_2_return )
-        @test( LOWDER.verify_index_imin(func_list, 3, fmin_y, y) == test_3_return )
-        @test( LOWDER.verify_index_imin(func_list, 4, fmin_y, y) == test_4_return )
+        
+        idx = 1
+        fi_y = 0.0
+
+        # Test 01
+        fmin, imin = LOWDER.fmin_partial_eval!( func_list, r, idx, fi_y, y, set )
+        @test( fmin == 0.0 )
+        @test( imin == 1 )
+        @test( set == [true, true, false, true] )
+
+        # Test parameters
+        idx = 2
+        fi_y = 0.0
+
+        # Test 02
+        fmin, imin = LOWDER.fmin_partial_eval!( func_list, r, idx, fi_y, y, set )
+        @test( fmin == 0.0 )
+        @test( imin == 2 )
+        @test( set == [true, true, false, true] )
+
+        # Test parameters
+        idx = 3
+        fi_y = 1.0
+
+        # Test 03
+        fmin, imin = LOWDER.fmin_partial_eval!( func_list, r, idx, fi_y, y, set )
+        @test( fmin == 0.0 )
+        @test( imin == 1 )
+        @test( set == [true, true, false, true] )
+
+        # Test parameters
+        idx = 4
+        fi_y = 0.0
+
+        # Test 04
+        fmin, imin = LOWDER.fmin_partial_eval!( func_list, r, idx, fi_y, y, set )
+        @test( fmin == 0.0 )
+        @test( imin == 4 )
+        @test( set == [true, true, false, true] )
+
+        # ---        
 
         # Test parameters
         y = [0.0, 1.0]
-        fmin_y = - 1.0
-        # Expected return
-        test_1_return = false
-        test_2_return = true
-        test_3_return = false
-        test_4_return = false
+        idx = 3
+        fi_y = 0.0
+
         # Test
-        @test( LOWDER.verify_index_imin(func_list, 1, fmin_y, y) == test_1_return )
-        @test( LOWDER.verify_index_imin(func_list, 2, fmin_y, y) == test_2_return )
-        @test( LOWDER.verify_index_imin(func_list, 3, fmin_y, y) == test_3_return )
-        @test( LOWDER.verify_index_imin(func_list, 4, fmin_y, y) == test_4_return )
+        fmin, imin = LOWDER.fmin_partial_eval!( func_list, r, idx, fi_y, y, set )
+        @test( fmin == -1.0 )
+        @test( imin == 2 )
+        @test( set == [false, true, false, false] )
+
+        # --- 
 
         # Test parameters
         y = [1.0, 1.0]
-        fmin_y = - 1.0
-        # Expected return
-        test_1_return = false
-        test_2_return = false
-        test_3_return = true
-        test_4_return = false
+        idx = 1
+        fi_y = 4.0
+
         # Test
-        @test( LOWDER.verify_index_imin(func_list, 1, fmin_y, y) == test_1_return )
-        @test( LOWDER.verify_index_imin(func_list, 2, fmin_y, y) == test_2_return )
-        @test( LOWDER.verify_index_imin(func_list, 3, fmin_y, y) == test_3_return )
-        @test( LOWDER.verify_index_imin(func_list, 4, fmin_y, y) == test_4_return )
+        fmin, imin = LOWDER.fmin_partial_eval!( func_list, r, idx, fi_y, y, set )
+        @test( fmin == -1.0 )
+        @test( imin == 3 )
+        @test( set == [false, false, true, false] )
+
+        # ---        
 
         # Test parameters
-        y = [0.0, - 1.0]
-        fmin_y = sin(- 1.0)
-        # Expected return
-        test_1_return = false
-        test_2_return = false
-        test_3_return = false
-        test_4_return = true
+        y = [0.0, -1.0]
+        idx = 1
+        fi_y = 0.0
+
         # Test
-        @test( LOWDER.verify_index_imin(func_list, 1, fmin_y, y) == test_1_return )
-        @test( LOWDER.verify_index_imin(func_list, 2, fmin_y, y) == test_2_return )
-        @test( LOWDER.verify_index_imin(func_list, 3, fmin_y, y) == test_3_return )
-        @test( LOWDER.verify_index_imin(func_list, 4, fmin_y, y) == test_4_return )
+        fmin, imin = LOWDER.fmin_partial_eval!( func_list, r, idx, fi_y, y, set )
+        @test( fmin == l(y) )
+        @test( imin == 4 )
+        @test( set == [false, false, false, true] )
 
     end
 
